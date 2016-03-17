d7d48a5a0e45d1d3541e97e35378b95c1e568d53
Stephane GINER <stephane.giner@gmail.com>
2016-03-17 07:41:20 +0100

    Ajout des services sur l'historique des sms

--- ovh.api/src/main/java/com/milleapplis/ovh/api/sms/SMSService.java
+++ ovh.api/src/main/java/com/milleapplis/ovh/api/sms/SMSService.java
@@ -11,6 +11,7 @@ import com.milleapplis.ovh.api.sms.enums.SMSWayTypeEnum;
 import com.milleapplis.ovh.api.sms.param.POSTSmsJobParam;
 import com.milleapplis.ovh.api.sms.param.POSTSmsSendersParam;
 import com.milleapplis.ovh.api.sms.result.SMSBlacklist;
+import com.milleapplis.ovh.api.sms.result.SMSIncoming;
 import com.milleapplis.ovh.api.sms.result.SMSPttDetails;
 import com.milleapplis.ovh.api.sms.result.SMSSender;
 import com.milleapplis.ovh.api.sms.result.SMSServiceName;
@@ -149,7 +150,72 @@ public class SMSService extends AbstractService {
 		}
 	}
 	
+	public Long getSMSServicenameException(String serviceName, String receiver) throws OVHApiException {
+		String url = String.format("sms/%s/exception?receiver=%d", serviceName, receiver);
+		
+		throw new OVHApiException(String.format("Service not implemented in java Api."));
+	}
 	
+	public List<Long> getSMSServicenameIncoming(String serviceName, Date creationDateTimeFrom, Date creationDateTimeTo, String sender, String tag) throws OVHApiException {
+		String url = String.format("sms/%s/document", serviceName);
+		String separator = "?";
+		if (creationDateTimeFrom != null) {
+			url += separator + "creationDatetime.from=" + creationDateTimeFrom;
+			separator = "&";
+		}
+		if (creationDateTimeTo != null) {
+			url += separator + "creationDatetime.to=" + creationDateTimeTo;
+			separator = "&";
+		}
+		if (tag != null) {
+			url += separator + "tag=" + tag;
+			separator = "&";
+		}
+		if (sender != null) {
+			url += separator + "sender=" + sender.toString();
+			separator = "&";
+		}
+		String result = executeService(Method.GET, url, "");
+		
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			return mapper.readValue(result, List.class);
+		}
+		catch (Exception e) {
+			throw new OVHApiException(String.format("Unable to call service %s", url), e);
+		}
+	}
+	
+	public SMSIncoming getSMSServicenameIncoming(String serviceName, String smsId) throws OVHApiException {
+		String url = String.format("sms/%s/incoming/%s", serviceName, smsId);
+		String result = executeService(Method.GET, url, "");
+		
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			return mapper.readValue(result, SMSIncoming.class);
+		}
+		catch (Exception e) {
+			throw new OVHApiException(String.format("Unable to call service /sms/%s/incoming/%s", serviceName, smsId), e);
+		}
+	}
+
+	public void DeleteSMSServicenameIncoming(String serviceName, String smsId) throws OVHApiException {
+		String url = String.format("sms/%s/incoming/%s", serviceName, smsId);
+		executeService(Method.DELETE, url, "");
+	}
+
+	public List<Long> getSMSServicenameJobs(String serviceName) throws OVHApiException {
+		String url = String.format("sms/%s/jobs", serviceName);
+		String result = executeService(Method.GET, url, "");
+		
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			return mapper.readValue(result, List.class);
+		}
+		catch (Exception e) {
+			throw new OVHApiException(String.format("Unable to call service %s", url), e);
+		}
+	}
 	
 	/**
 	 * Service permettant d'envoyer des SMS
---
+++ ovh.api/src/main/java/com/milleapplis/ovh/api/sms/result/SMSIncoming.java
@@ -0,0 +1,64 @@
+package com.milleapplis.ovh.api.sms.result;
+
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class SMSIncoming {
+
+	@JsonProperty
+	private double credits;
+	@JsonProperty
+	private String sender;
+	@JsonProperty
+	private Date creationDatetime;
+	@JsonProperty
+	private long id;
+	@JsonProperty
+	private String tag;
+	@JsonProperty
+	private String message;
+	
+	public String toString() {
+		return String.format("[Credit : %s][Sender : %s][Creation date : ‰s][Tag : %][Message : %s]", credits, sender, creationDatetime, tag, message);
+	}
+	
+	public double getCredits() {
+		return credits;
+	}
+	public void setCredits(double credits) {
+		this.credits = credits;
+	}
+	public String getSender() {
+		return sender;
+	}
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+	public Date getCreationDatetime() {
+		return creationDatetime;
+	}
+	public void setCreationDatetime(Date creationDatetime) {
+		this.creationDatetime = creationDatetime;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTag() {
+		return tag;
+	}
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+}
